4a407f316588c72f53133fae9c4a077c58f45289
tbwebb22 <tbwebb22@gmail.com>
2022-10-19 11:02:00 -0400

    Remove old interfaces

--- contracts/interfaces/IGnosisWrapper.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface IGnosisWrapper {
-    function initialize(address _accessControl, address _gnosisSafe) external;
-    function gnosisSafe() external view returns(address);
-}
