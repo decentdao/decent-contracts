82fff0248bcaf5cab28d47a3b029ab5dc668c766
Kirill Klimenko <klimenkomud@gmail.com>
2022-10-31 17:22:33 +0200

    Add FractalUsul contract, add Usul package to dependencies

---
+++ contracts/FractalUsul.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// Usul was previously named "Seele" and SekerDAO was TokenWalk
+// that's where this naming changes are coming from
+import "@tokenwalk/seele/contracts/Usul.sol";
+
+abstract contract FractalUsul is Usul {
+  event ProposalMetadataCreated(uint256 proposalId, string title, string description);
+
+  function submitProposalWithMetaData(
+        bytes32[] memory txHashes,
+        address strategy,
+        bytes memory data,
+        string calldata title,
+        string calldata description
+  ) external {
+        require(
+            isStrategyEnabled(strategy),
+            "voting strategy is not enabled for proposal"
+        );
+        require(txHashes.length > 0, "proposal must contain transactions");
+        proposals[totalProposalCount].txHashes = txHashes;
+        proposals[totalProposalCount].strategy = strategy;
+        totalProposalCount++;
+        IStrategy(strategy).receiveProposal(
+            abi.encode(totalProposalCount - 1, txHashes, data)
+        );
+        emit ProposalCreated(strategy, totalProposalCount - 1, msg.sender);
+        emit ProposalMetadataCreated(totalProposalCount - 1, title, description);
+  }
+}
