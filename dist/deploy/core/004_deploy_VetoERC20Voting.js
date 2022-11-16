cf23eaf940837b29e7d139d6565bb4caa215afa3
sethhrbek <seth.hrbek@gmail.com>
2022-11-16 15:39:20 -0500

    add dist to gitignore and remove dist from repo

--- dist/deploy/core/004_deploy_VetoERC20Voting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const deployNonUpgradeable_1 = require("../helpers/deployNonUpgradeable");
-const func = async (hre) => {
-    await (0, deployNonUpgradeable_1.deployNonUpgradeable)(hre, "VetoERC20Voting", []);
-};
-exports.default = func;
--- dist/test/getInterfaceSelector.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ethers_1 = require("ethers");
-const getInterfaceSelector = (iface) => {
-    return Object.keys(iface.functions)
-        .reduce((p, c) => p.xor(ethers_1.BigNumber.from(iface.getSighash(iface.functions[c]))), ethers_1.BigNumber.from(0))
-        .toHexString();
-};
-exports.default = getInterfaceSelector;
