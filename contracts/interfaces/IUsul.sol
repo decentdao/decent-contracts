b31d74acc2df36bed7e547c2d82075157f5b886f
tbwebb22 <tbwebb22@gmail.com>
2022-11-16 15:43:37 -0500

    Add function to IUsul interface

--- contracts/interfaces/IUsul.sol
+++ contracts/interfaces/IUsul.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+
 interface IUsul {
     /// @dev Returns the hash of a transaction in a proposal.
     /// @param proposalId the proposal to inspect.
@@ -10,4 +12,16 @@ interface IUsul {
         external
         view
         returns (bytes32);
+
+    function getTransactionHash(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation
+    ) external view returns (bytes32);
+
+    /// @dev Get the state of a proposal
+    /// @param proposalId the identifier of the proposal
+    /// @return ProposalState the enum of the state of the proposal
+    function state(uint256 proposalId) external view returns (uint256);
 }
