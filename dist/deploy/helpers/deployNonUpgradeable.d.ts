7dd28af6cf62bfd9711730ae5088531df6f3497c
Christopher Dancy <chris.dancy@decentlabs.io>
2022-11-17 16:06:40 -0500

    remove dist file

--- dist/deploy/helpers/deployNonUpgradeable.d.ts
+++
@@ -1,3 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-declare const deployNonUpgradeable: (hre: HardhatRuntimeEnvironment, contractName: string, args?: any[]) => Promise<void>;
-export { deployNonUpgradeable };
--- dist/test/getInterfaceSelector.d.ts
+++
@@ -1,3 +0,0 @@
-import { utils } from "ethers";
-declare const getInterfaceSelector: (iface: utils.Interface) => string;
-export default getInterfaceSelector;
