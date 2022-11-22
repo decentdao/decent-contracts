92b13ab4777584c3667fa7359eee7f04d3c4f6da
tbwebb22 <tbwebb22@gmail.com>
2022-11-22 09:55:00 -0500

    Remove unused function from interface

--- contracts/interfaces/IOZLinearVoting.sol
+++ contracts/interfaces/IOZLinearVoting.sol
@@ -10,15 +10,4 @@ interface IOZLinearVoting {
     /// @param proposalId the proposal to vote for.
     /// @return boolean.
     function isPassed(uint256 proposalId) external view returns (bool);
-
-    /// @dev Returns proposal data
-    /// @param proposalId The proposal to get data for
-    /// @return yesVotes Quantity of yes votes for the proposal
-    /// @return noVotes Quantity of no votes for the proposal
-    /// @return abstainVotes Quantity of abstain votes for the proposal
-    /// @return deadline The timestamp when voting ends
-    /// @return startBlock The block number when voting begins
-    function proposals(uint256 proposalId) external view returns (
-      uint256 yesVotes, uint256 noVotes, uint256 abstainVotes, uint256 deadline, uint256 startBlock
-    );
 }
