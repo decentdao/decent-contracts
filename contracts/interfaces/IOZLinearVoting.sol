090416bce89da66d102d7ef6f657dd6e5a335258
tbwebb22 <tbwebb22@gmail.com>
2022-11-22 09:45:58 -0500

    Add natspec

--- contracts/interfaces/IOZLinearVoting.sol
+++ contracts/interfaces/IOZLinearVoting.sol
@@ -11,6 +11,13 @@ interface IOZLinearVoting {
     /// @return boolean.
     function isPassed(uint256 proposalId) external view returns (bool);
 
+    /// @dev Returns proposal data
+    /// @param proposalId The proposal to get data for
+    /// @return yesVotes Quantity of yes votes for the proposal
+    /// @return noVotes Quantity of no votes for the proposal
+    /// @return abstainVotes Quantity of abstain votes for the proposal
+    /// @return deadline The timestamp when voting ends
+    /// @return startBlock The block number when voting begins
     function proposals(uint256 proposalId) external view returns (
       uint256 yesVotes, uint256 noVotes, uint256 abstainVotes, uint256 deadline, uint256 startBlock
     );
