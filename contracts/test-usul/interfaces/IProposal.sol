fb1c47ae775e7f2a1e650aad62ae52ba141f7286
tbwebb22 <tbwebb22@gmail.com>
2022-11-22 18:05:26 -0500

    Remove all contracts copied from Usul repo

--- contracts/test-usul/interfaces/IProposal.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-
-pragma solidity ^0.8.6;
-
-interface IProposal {
-    function receiveStrategy(uint256 proposalId, uint256 timeLockPeriod)
-        external;
-}
--- contracts/test-usul/interfaces/IStrategy.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-
-pragma solidity ^0.8.6;
-
-interface IStrategy {
-    function receiveProposal(bytes memory data) external;
-}
