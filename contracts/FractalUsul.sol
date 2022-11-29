0ecc99c10ec0fd13058cba2d1eee328f96e1d323
Kirill Klimenko <klimenkomud@gmail.com>
2022-11-29 17:20:10 +0200

    Emit data with transactions instead of storing them

--- contracts/FractalUsul.sol
+++ contracts/FractalUsul.sol
@@ -5,37 +5,27 @@ pragma solidity ^0.8.0;
 // that's where this naming differences are coming from
 import "@tokenwalk/seele/contracts/Usul.sol";
 
-abstract contract FractalUsul is Usul {
+contract FractalUsul is Usul {
   struct Transaction {
     address to;
     uint256 value;
     bytes data;
     Enum.Operation operation;
   }
-  
-  struct Proposal {
-    bool canceled;
-    uint256 timeLockPeriod; // queue period for safety
-    bytes32[] txHashes;
-    Transaction[] transactions;
-    uint256 executionCounter;
-    address strategy; // the module that is allowed to vote on this
-  }
 
   event ProposalMetadataCreated(
     uint256 proposalId, 
-    string title, 
+    Transaction[] transactions,
+    string title,
     string description, 
     string documentationUrl
   );
-
-  // mapping of proposal id to proposal
-  mapping(uint256 => Proposal) public override proposals;
 
   /// @dev This method is used instead of Usul.submitProposal. Essentially - it just implements same behavior
   /// but then - it also emits metadata of the proposal in ProposalMetadataCreated event.
   function submitProposalWithMetaData(
         Transaction[] memory transactions,
+        bytes32[] memory txHashes,
         address strategy,
         bytes memory data,
         string calldata title,
@@ -46,29 +36,21 @@ abstract contract FractalUsul is Usul {
           isStrategyEnabled(strategy),
           "voting strategy is not enabled for proposal"
       );
-      require(transactions.length > 0, "proposal must contain transactions");
-      bytes32[] memory txHashes;
-
-      for (uint256 i = 0; i < transactions.length; i++) {
-        Transaction memory currentTx = transactions[i];
-        bytes32 txHash = getTransactionHash(
-          currentTx.to, 
-          currentTx.value,
-          currentTx.data, 
-          currentTx.operation
-        );
-
-        txHashes[i] = txHash;
-      }
+      require(txHashes.length > 0, "proposal must contain transactions");
 
       proposals[totalProposalCount].txHashes = txHashes;
-      proposals[totalProposalCount].transactions = transactions;
       proposals[totalProposalCount].strategy = strategy;
       totalProposalCount++;
       IStrategy(strategy).receiveProposal(
-          abi.encode(totalProposalCount - 1, transactions, data)
+          abi.encode(totalProposalCount - 1, txHashes, data)
       );
       emit ProposalCreated(strategy, totalProposalCount - 1, msg.sender);
-      emit ProposalMetadataCreated(totalProposalCount - 1, title, description, documentationUrl);
+      emit ProposalMetadataCreated(
+        totalProposalCount - 1, 
+        transactions, 
+        title, 
+        description, 
+        documentationUrl
+      );
   }
 }
