19eacfe0160a26d54fba8b50fa9c4b7c79d33df7
tbwebb22 <tbwebb22@gmail.com>
2023-02-03 09:32:16 -0500

    Remove Usul tests

--- contracts/usul/interfaces/IStrategy.sol
+++ contracts/usul/interfaces/IStrategy.sol
@@ -3,5 +3,11 @@
 pragma solidity ^0.8.6;
 
 interface IStrategy {
+    /// @dev Called by the proposal module, this notifes the strategy of a new proposal.
+    /// @param data any extra data to pass to the voting strategy
     function receiveProposal(bytes memory data) external;
+
+    /// @dev Calls the proposal module to notify that a quorum has been reached.
+    /// @param proposalId the proposal to vote for.
+    function queueProposal(uint256 proposalId) external;
 }
