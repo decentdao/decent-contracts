09c871bec964ba6dfcf7bb0e11622887f62e06f4
herbig <mikeherbig@gmail.com>
2023-03-14 10:59:49 -0400

    remove unused timestamp stuff

--- test/time.ts
+++ test/time.ts
@@ -10,26 +10,9 @@ const advanceBlock = async () => {
   await ethers.provider.send("evm_mine", []);
 };
 
-const latest = async () => {
-  const block = await ethers.provider.getBlock("latest");
-  return block.timestamp;
-};
-
-const increase = async (duration: number) => {
-  await increaseTo((await latest()) + duration);
-};
-
-const increaseTo = async (to: number) => {
-  await ethers.provider.send("evm_setNextBlockTimestamp", [to]);
-  await advanceBlock();
-};
-
 const defaultExport = {
   advanceBlocks,
   advanceBlock,
-  latest,
-  increase,
-  increaseTo,
 };
 
 export default defaultExport;
