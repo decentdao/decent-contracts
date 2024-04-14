4d402eb55501deadc8c58ff1b88e33ee95f8b4f9
Adam Gall <galladamj@gmail.com>
2024-04-14 13:55:11 -0400

    Update project to use current Hardhat, remove bunch of extra tools

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,11 @@
 import * as dotenv from "dotenv";
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-deploy";
-import "@typechain/hardhat";
-import "hardhat-tracer";
-import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-dependency-compiler";
-import "hardhat-gas-reporter";
+import "hardhat-deploy";
 import "solidity-docgen";
 
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -35,11 +16,6 @@ const config: HardhatUserConfig = {
         runs: 200,
       },
     },
-  },
-  gasReporter: {
-    enabled: true,
-    outputFile: "gas-report.txt",
-    noColors: true,
   },
   dependencyCompiler: {
     paths: [
