4d75b601407377e2427918ff140788532f0434ad
Adam Gall <galladamj@gmail.com>
2024-06-15 00:22:00 -0400

    Modify structure of published metadata again

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -6,20 +6,32 @@ npm run test
 
 npx hardhat export --export-all deployments.json
 
-jq 'walk(if type == "object" then del(.abi, .name, .chainId) else . end)' deployments.json > temp_addresses.json
-jq 'to_entries | map({key: .key, value: .value[0]}) | from_entries' temp_addresses.json > addresses.json
-echo "export default $(cat addresses.json) as const;" > publish/addresses.ts
-rm temp_addresses.json addresses.json
+rm index.ts
+rm -rf publish && mkdir publish
 
-jq '."1"[0].contracts' deployments.json > temp_abis.json
-jq 'to_entries | map({key: .key, value: {abi: .value.abi}}) | from_entries' temp_abis.json > abis.json
+# Step 1: Remove the `abi`, `name`, and `chainId` keys
+jq 'walk(if type == "object" then del(.abi, .name, .chainId) else . end)' deployments.json > temp_addresses.json
+# Step 2: Replace array with its single object item
+jq 'to_entries | map({key: .key, value: .value[0]}) | from_entries' temp_addresses.json > temp_flat_addresses.json
+# Step 3: Flatten the structure to remove bottom level objects
+jq 'to_entries | map({key: .key, value: .value.contracts | map_values(.address)}) | from_entries' temp_flat_addresses.json > addresses.json
+# Step 4: Wrap the JSON content in TypeScript format and append "as const;"
+echo "export default $(cat addresses.json) as const;" > publish/addresses.ts
+# Step 5: Cleanup
+rm temp_addresses.json temp_flat_addresses.json addresses.json
+
+
+# Step 1: Extract `abi` values directly under each contract key
+jq '."1"[0].contracts | to_entries | map({key: .key, value: .value.abi}) | from_entries' deployments.json > abis.json
+# Step 2: Wrap the JSON content in TypeScript format and append "as const;"
 echo "export default $(cat abis.json) as const;" > publish/abis.ts
-rm temp_abis.json abis.json
+# Step 3: Cleanup
+rm abis.json
 
 rm deployments.json 
 
 cat << EOF > index.ts
-import abis from "./publish/abis.ts";
-import addresses from "./publish/addresses.ts";
+import abis from "./publish/abis";
+import addresses from "./publish/addresses";
 export { abis, addresses };
 EOF
