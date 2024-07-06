57f9f69c41a5f34562de7ebe2dcec082e1e2745f
Adam Gall <adam@decentdao.org>
2024-07-06 01:30:41 -0400

    Mainnet polygon deployment cleanup (#96)
    * No more deploying LinearERC20Voting on mainnet and polygon when running deployment script.
    
    * Include the original LinearERC20Voting contract which is deployed to mainnet and polygon

--- deploy/core/010_deploy_LinearERC20Voting.ts
+++ deploy/core/010_deploy_LinearERC20Voting.ts
@@ -3,6 +3,13 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const chainId = await hre.getChainId();
+
+  // See https://github.com/decentdao/decent-contracts/pull/96
+  if (chainId === "1" || chainId === "137") {
+    return;
+  }
+
   await deployNonUpgradeable(hre, "LinearERC20Voting", []);
 };
 
