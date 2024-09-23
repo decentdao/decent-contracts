9e66612791035f746e344fce1e3ea551f90e1f5e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-09-23 13:36:08 -0400

    create AA Hat

--- contracts/DecentAutonomousAdminHat.sol
+++ contracts/DecentAutonomousAdminHat.sol
@@ -2,52 +2,133 @@
 pragma solidity =0.8.19;
 import "./interfaces/hats/IHats.sol";
 import "./interfaces/hats/IHatsElectionEligibility.sol";
+import "./interfaces/sablier/ISablierV2LockupLinear.sol";
 
 contract DecentAutonomousAdminHat {
     string public constant NAME = "DecentAutonomousAdminHat";
 
-    uint256 public hatId;
+    uint256 public adminHatId;
 
+    struct SablierStreamInfo {
+        uint256 streamId;
+        ISablierV2LockupLinear sablierV2LockupLinear;
+    }
     struct TriggerStartArgs {
         address userHatSmartAccountAddress;
-        IHats userHatPropocol;
+        address currentWearer;
+        IHats userHatProtocol;
         uint256 userHatId;
+        address nominatedWearer;
+        SablierStreamInfo[] sablierStreamInfo;
     }
 
     // //////////////////////////////////////////////////////////////
     //                         Constructor
     // //////////////////////////////////////////////////////////////
-    constructor() {}
-
-    // //////////////////////////////////////////////////////////////
-    //                         Initializer
-    // //////////////////////////////////////////////////////////////
-
-    /**
-     *
-     * @param _initData encoded initialization parameters: `uint256 hatId`
-     */
-
-    function _setup(bytes calldata _initData) internal {
-        (hatId) = abi.decode(_initData, (uint256));
+    constructor(uint256 _adminHatId) {
+        adminHatId =_adminHatId;
     }
 
     // //////////////////////////////////////////////////////////////
     //                         Public Functions
     // //////////////////////////////////////////////////////////////
     function triggerStartNextTerm(TriggerStartArgs calldata args) public {
-        // ? should we use `checkHatWearerStatus` here?
-        // ? should we use `isAdminOfHat` here?
-        // ? should we use `isEligible` here?
-
-        address hatsEE = args.userHatPropocol.getHatEligibilityModule(
-            args.userHatId
+        require(
+            args.userHatProtocol.isWearerOfHat(
+                args.currentWearer,
+                args.userHatId
+            ),
+            "Not current wearer"
         );
 
-        IHatsElectionEligibility(hatsEE).startNextTerm();
+        address hatsEligibilityModuleAddress = args
+            .userHatProtocol
+            .getHatEligibilityModule(args.userHatId);
+
+        IHatsElectionEligibility hatsElectionModule = IHatsElectionEligibility(
+            hatsEligibilityModuleAddress
+        );
+
+        hatsElectionModule.startNextTerm();
+
+        // transfer user hat to self
+        args.userHatProtocol.transferHat(
+            args.userHatId,
+            args.currentWearer,
+            address(this)
+        );
+
+        // for each withdrawable stream, withdraw funds to current wearer of hat
+        _flushUnclaimedFunds(
+            _getStreamsWithUnclaimedFunds(args.sablierStreamInfo),
+            args.currentWearer
+        );
+
+        // transfer hat to nominated wearer
+        args.userHatProtocol.transferHat(
+            args.userHatId,
+            address(this),
+            args.nominatedWearer
+        );
     }
 
     // //////////////////////////////////////////////////////////////
     //                         Internal Functions
     // //////////////////////////////////////////////////////////////
+
+    /**
+     * @dev Withdraws unclaimed funds from Sablier streams.
+     * @param _sablierStreamInfo SablierStreamInfo array
+     */
+    function _flushUnclaimedFunds(
+        SablierStreamInfo[] memory _sablierStreamInfo,
+        address withdrawTo
+    ) internal {
+        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
+            _sablierStreamInfo[i].sablierV2LockupLinear.withdrawMax(
+                _sablierStreamInfo[i].streamId,
+                withdrawTo
+            );
+        }
+    }
+
+    /**
+     * @dev Returns an array of Sablier stream ids that have unclaimed funds.
+     * @param _sablierStreamInfo SablierStreamInfo array
+     * @return streamsWithUnclaimedFunds An array of SablierStreamInfo that have unclaimed funds
+     */
+    function _getStreamsWithUnclaimedFunds(
+        SablierStreamInfo[] memory _sablierStreamInfo
+    ) internal view returns (SablierStreamInfo[] memory) {
+        uint256 streamsWithUnclaimedFundsCount = 0;
+
+        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
+            uint128 withdrawableAmount = _sablierStreamInfo[i]
+                .sablierV2LockupLinear
+                .withdrawableAmountOf(_sablierStreamInfo[i].streamId);
+
+            if (withdrawableAmount > 0) {
+                streamsWithUnclaimedFundsCount++;
+            }
+        }
+
+        SablierStreamInfo[]
+            memory streamsWithUnclaimedFunds = new SablierStreamInfo[](
+                streamsWithUnclaimedFundsCount
+            );
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
+            uint128 withdrawableAmount = _sablierStreamInfo[i]
+                .sablierV2LockupLinear
+                .withdrawableAmountOf(_sablierStreamInfo[i].streamId);
+
+            if (withdrawableAmount > 0) {
+                streamsWithUnclaimedFunds[index] = _sablierStreamInfo[i];
+                index++;
+            }
+        }
+
+        return streamsWithUnclaimedFunds;
+    }
 }
