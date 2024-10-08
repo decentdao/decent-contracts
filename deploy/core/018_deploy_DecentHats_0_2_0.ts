1bafbf844a258db50bf78e209cbf62e197c065cf
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-10-07 22:46:39 -0400

    fix deployment script

--- deploy/core/018_deploy_DecentHats_0_2_0.ts
+++ deploy/core/018_deploy_DecentHats_0_2_0.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  await deployNonUpgradeable(hre, "DecentHats_0_2_0", ["0.2.0"]);
+  await deployNonUpgradeable(hre, "DecentHats_0_2_0");
 };
 
 export default func;
