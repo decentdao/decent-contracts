ee3478cc5b76e4bc66a2cc7eb02c73ac0849c118
Adam Gall <galladamj@gmail.com>
2024-10-09 17:27:23 -0400

    Update Sablier contracts to their actual solidity pragma

--- contracts/interfaces/sablier/full/IERC4096.sol
+++ contracts/interfaces/sablier/full/IERC4096.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC4906.sol)
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
