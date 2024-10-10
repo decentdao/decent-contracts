90c84119dea31e2864942084b7446c83ea96077b
Adam Gall <galladamj@gmail.com>
2024-10-10 16:31:01 -0400

    More comment out unused stuff

--- deploy/core/011_deploy_ModuleProxyFactory.ts
+++ deploy/core/011_deploy_ModuleProxyFactory.ts
@@ -1,8 +1,8 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 // import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 
-const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+const func: DeployFunction = async (/* hre: HardhatRuntimeEnvironment */) => {
   // No longer deploying ModuleProxyFactory to any new networks..
   // This contract is deployed by the Zodiac team.
   // await deployNonUpgradeable(hre, "ModuleProxyFactory", []);
