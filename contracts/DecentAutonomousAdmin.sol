075a281442a4f32ba79721491528958762807fc2
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-10-17 18:58:08 -0400

    remove flushing of payments when triggering new term

--- contracts/DecentAutonomousAdmin.sol
+++ contracts/DecentAutonomousAdmin.sol
@@ -18,7 +18,6 @@ contract DecentAutonomousAdmin {
         IHats userHatProtocol;
         uint256 userHatId;
         address nominatedWearer;
-        SablierStreamInfo[] sablierStreamInfo;
     }
 
     // //////////////////////////////////////////////////////////////
@@ -48,83 +47,14 @@ contract DecentAutonomousAdmin {
         hatsElectionModule.startNextTerm();
 
         // transfer user hat to self
-        args.userHatProtocol.transferHat(
+        args.userHatProtocol.checkHatWearerStatus(
             args.userHatId,
-            args.currentWearer,
-            address(this)
-        );
-
-        // for each withdrawable stream, withdraw funds to current wearer of hat
-        _flushUnclaimedFunds(
-            _getStreamsWithUnclaimedFunds(args.sablierStreamInfo),
             args.currentWearer
         );
-
-        // transfer hat to nominated wearer
-        args.userHatProtocol.transferHat(
-            args.userHatId,
-            address(this),
-            args.nominatedWearer
-        );
+        args.userHatProtocol.mintHat(args.userHatId, args.nominatedWearer);
     }
 
     // //////////////////////////////////////////////////////////////
     //                         Internal Functions
     // //////////////////////////////////////////////////////////////
-
-    /**
-     * @dev Withdraws unclaimed funds from Sablier streams.
-     * @param _sablierStreamInfo SablierStreamInfo array
-     */
-    function _flushUnclaimedFunds(
-        SablierStreamInfo[] memory _sablierStreamInfo,
-        address withdrawTo
-    ) internal {
-        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
-            _sablierStreamInfo[i].sablierV2Lockup.withdrawMax(
-                _sablierStreamInfo[i].streamId,
-                withdrawTo
-            );
-        }
-    }
-
-    /**
-     * @dev Returns an array of Sablier stream ids that have unclaimed funds.
-     * @param _sablierStreamInfo SablierStreamInfo array
-     * @return streamsWithUnclaimedFunds An array of SablierStreamInfo that have unclaimed funds
-     */
-    function _getStreamsWithUnclaimedFunds(
-        SablierStreamInfo[] memory _sablierStreamInfo
-    ) internal view returns (SablierStreamInfo[] memory) {
-        uint256 streamsWithUnclaimedFundsCount = 0;
-
-        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
-            uint128 withdrawableAmount = _sablierStreamInfo[i]
-                .sablierV2Lockup
-                .withdrawableAmountOf(_sablierStreamInfo[i].streamId);
-
-            if (withdrawableAmount > 0) {
-                streamsWithUnclaimedFundsCount++;
-            }
-        }
-
-        SablierStreamInfo[]
-            memory streamsWithUnclaimedFunds = new SablierStreamInfo[](
-                streamsWithUnclaimedFundsCount
-            );
-        uint256 index = 0;
-
-        for (uint256 i = 0; i < _sablierStreamInfo.length; i++) {
-            uint128 withdrawableAmount = _sablierStreamInfo[i]
-                .sablierV2Lockup
-                .withdrawableAmountOf(_sablierStreamInfo[i].streamId);
-
-            if (withdrawableAmount > 0) {
-                streamsWithUnclaimedFunds[index] = _sablierStreamInfo[i];
-                index++;
-            }
-        }
-
-        return streamsWithUnclaimedFunds;
-    }
 }
