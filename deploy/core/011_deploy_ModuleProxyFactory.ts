78e415139e9666b9ac7db1078fbd9bf0f1288aae
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-10-21 11:36:28 -0400

    revert changes

--- deploy/core/011_deploy_ModuleProxyFactory.ts
+++ deploy/core/011_deploy_ModuleProxyFactory.ts
@@ -1,11 +1,11 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types"
 // import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 
-const func: DeployFunction = async (_: HardhatRuntimeEnvironment) => {
+const func: DeployFunction = async (/* hre: HardhatRuntimeEnvironment */) => {
   // No longer deploying ModuleProxyFactory to any new networks..
   // This contract is deployed by the Zodiac team.
   // await deployNonUpgradeable(hre, "ModuleProxyFactory", []);
-};
+}
 
-export default func;
+export default func
